648920c53507bddb5c5c9001343bff2c76eb3229
yosi475 <yosi475@naver.com>
2026-05-22 09:29:19 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -2,7 +2,22 @@
 #include <string.h>
 
 int main(int argc, const char* argv[], const char* envp[]) {
+    FILE* fp = fopen("student.txt", "w");
+    if (fp == NULL)
+        return 0;
 
+    while (1) {
+        char name[10] = {0};
+        int score = 0;
+
+        scanf("%s", name);
+        if (strcmp(name, "quit") == 0)
+            break;
+
+        scanf("%d", &score);
+        fprintf(fp, "%s %d\n", name, score);
+    }
+
+    fclose(fp);
     return 0;
 }
-
