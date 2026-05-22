9a237c922e5de3964bdc465c7915e925614db1c7
yosi475 <yosi475@naver.com>
2026-05-22 09:50:12 +0900

    Update 2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -4,6 +4,22 @@ int main(int argc, const char* argv[]) {
     if (argc < 3)
         return 0;
 
+    FILE* fp_src = fopen(argv[1], "r");
+    if (fp_src == NULL)
+        return 0;
 
+    FILE* fp_dst = fopen(argv[2], "w");
+    if (fp_dst == NULL) {
+        fclose(fp_src);
+        return 0;
+    }
+
+    int c;
+    while ((c = fgetc(fp_src)) != EOF) {
+        fputc(c, fp_dst);
+    }
+
+    fclose(fp_src);
+    fclose(fp_dst);
+    return 0;
 }
-
