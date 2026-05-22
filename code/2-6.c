e41dbd77fa9d2a4540264fdd892c4bd7624233d8
yosi475 <yosi475@naver.com>
2026-05-22 10:12:53 +0900

    Update 2-6.c

--- code/2-6.c
+++ code/2-6.c
@@ -6,10 +6,18 @@ int main(int argc, const char* argv[]) {
         return 0;
 
     FILE* fp = fopen(argv[1], "r");
+    if (fp == NULL)
+        return 0;
+
     const char* target_str = argv[2];
+    char buffer[512];
 
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        if (strstr(buffer, target_str) != NULL) {
+            printf("%s", buffer);
+        }
+    }
 
-    
     fclose(fp);
+    return 0;
 }
-
