56fcb56f57bce98b6d77f819e12931b3d1168840
yosi475 <yosi475@naver.com>
2026-05-22 09:40:01 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -5,9 +5,24 @@ int main(int argc, const char* argv[]) {
         return 0;
 
     FILE* fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        return 0;
+    }
 
+    int alphabet[26] = {0};
+    int c;
 
+    while ((c = fgetc(fp)) != EOF) {
+        if (c >= 'a' && c <= 'z') {
+            alphabet[c - 'a']++;
+        }
+    }
+
+    for (int i = 0; i < 26; i++) {
+        printf("%c: %d\n", 'a' + i, alphabet[i]);
+    }
 
     fclose(fp);
+    
+    return 0;
 }
-
