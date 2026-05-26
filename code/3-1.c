9a7e0ae7ecfeb9c6354fb371ca5bb9a56ec95fb2
yosi475 <yosi475@naver.com>
2026-05-26 12:04:28 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 int main(int argc, const char* argv[]) {
-    if (argc < 2) 
+    if (argc < 2)
         return 0;
-    
+
     FILE* fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        perror("file does not exist");
+        return 1;
+    }
 
-
+    fclose(fp);
     return 0;
 }
